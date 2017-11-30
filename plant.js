c97ab417b793e50a45a24b94e14a6e919bcd1cfb
speeeee <snsallay@gmail.com>
2017-11-29 21:18:36 -0800

    new project

---
+++ plant.js
@@ -0,0 +1,108 @@
+var vss = `#version 300 es
+in vec4 a_pos;
+uniform vec4 u_disp;
+
+uniform mat4 u_model; uniform mat4 u_view; uniform mat4 u_proj;
+
+void main() { gl_Position = u_proj*u_view*u_model*(a_pos+u_disp); }
+`;
+
+var fss = `#version 300 es
+precision mediump float;
+out vec4 outc;
+
+void main() {
+  outc = vec4(1,0,0.5,1); }
+`;
+
+function createShader(gl, type, sc) {
+  var shader = gl.createShader(type);
+  gl.shaderSource(shader,sc);
+  gl.compileShader(shader);
+  var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if(success) { return shader; }
+  console.log(gl.getShaderInfoLog(shader));
+  gl.deleteShader(shader); }
+
+function createProgram(gl,vs,fs) {
+  var program = gl.createProgram();
+  gl.attachShader(program,vs); gl.attachShader(program,fs);
+  gl.linkProgram(program);
+  var success = gl.getProgramParameter(program,gl.LINK_STATUS);
+  if(success) { return program; }
+  console.log(gl.getProgramInfoLog(program)); gl.deleteProgram(program); }
+
+function resize(canvas,perspective) {
+  var disp_width = canvas.clientWidth;
+  var disp_height = canvas.clientHeight;
+  if(disp_width!=canvas.width||disp_height!=canvas.height) { canvas.width = disp_width; canvas.height = disp_height;
+    var ratio = canvas.width/canvas.height;
+    perspective = mat4.frustum_pers(-ratio,ratio,-1,1,1,500); } }
+
+// draw //
+function draw(perspective) {
+  resize(gl.canvas,perspective);
+
+  gl.viewport(0,0,gl.canvas.width,gl.canvas.height);
+  gl.useProgram(program);
+  gl.clearColor(0,0,0,0);
+  gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
+
+  gl.bindVertexArray(vao);
+
+  gl.drawArrays(gl.TRIANGLES,0,3); }
+
+// main //
+var canvas = document.getElementById("c");
+var gl = canvas.getContext("webgl2");
+
+gl.enable(gl.DEPTH_TEST);
+
+var model = [1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1];
+var view = [1,0,0,0, 0,1,0,0, 0,0,1,-2.5, 0,0,0,1];
+var ratio = gl.canvas.clientWidth/gl.canvas.clientHeight;
+var perspective = mat4.frustum_pers(-ratio,ratio,-1,1,1,500);
+console.log(perspective); console.log(ratio);
+
+var vs = createShader(gl,gl.VERTEX_SHADER,vss);
+var fs = createShader(gl,gl.FRAGMENT_SHADER,fss);
+var program = createProgram(gl,vs,fs);
+
+var pos_attrib = gl.getAttribLocation(program, "a_pos");
+var pos_buf = gl.createBuffer();
+
+gl.bindBuffer(gl.ARRAY_BUFFER, pos_buf);
+
+var pos = [0,0,0, 0,0.5,0, 0.7,0,0];
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(pos), gl.STATIC_DRAW);
+
+var vao = gl.createVertexArray();
+gl.bindVertexArray(vao);
+gl.enableVertexAttribArray(pos_attrib);
+
+gl.vertexAttribPointer(pos_attrib,3,gl.FLOAT,false,0,0);
+
+gl.useProgram(program);
+
+var disp_loc = gl.getUniformLocation(program,"u_disp");
+var model_loc = gl.getUniformLocation(program,"u_model");
+gl.uniformMatrix4fv(model_loc,true,model);
+var view_loc = gl.getUniformLocation(program,"u_view");
+gl.uniformMatrix4fv(view_loc,true,view);
+var proj_loc = gl.getUniformLocation(program,"u_proj");
+gl.uniformMatrix4fv(proj_loc,true,perspective);
+gl.uniform4f(disp_loc,0,0,0,1);
+
+draw(perspective);
+
+// controls //
+var omx = 0; var omy = 0;
+var camera = { x: 0, y: 0 };
+canvas.addEventListener("mousemove", function(evt) {
+  if(omx==0&&omy==0) { omx = evt.clientX; omy = evt.clientY; return; }
+  else { var nm = { x: evt.clientX - omx, y: evt.clientY - omy };
+    camera = { x: camera.x+nm.x*0.001, y: camera.y+nm.y*0.001 };
+    //gl.uniform4f(disp_loc,camera.x,camera.y,0,1);
+    model= mat4.smul(mat4.rotate(0.001,0,1,0),model,4);
+    gl.uniformMatrix4fv(model_loc,true,model);
+    omx = evt.clientX; omy = evt.clientY; draw(perspective); } });
